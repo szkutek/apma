f8f3474c73348874b699841a061819c712366774
szkutek <aga.szkutek@gmail.com>
2017-10-13 22:51:10 +0200

    before optimization

--- sem2/agent-based modelling/l1/forest_fire.py
+++ sem2/agent-based modelling/l1/forest_fire.py
@@ -2,13 +2,11 @@ import numpy as np
 import random as rnd
 
 
-# 't' tree; '*' fire; '_' empty; '.' burned tree
+# 'T' tree; 'X' fire; ' ' empty; '_' burned tree
 # 1 tree; 2 fire; 0 empty; 3 burned tree
-# TODO is state necessary?
 
-def forest_fire(p, L):
-    f, s = (start_fire(make_forest(L, p)))
-    return automaton(f, s)
+def forest_fire(p, L, direction='0', strength=1):
+    return automaton(start_fire(make_forest(L, p)), direction, strength)
 
 
 def make_forest(L=10, p=1.0):
@@ -18,62 +16,70 @@ def make_forest(L=10, p=1.0):
     return pom
 
 
-def print_forest(forest, state=0):
+def print_forest(forest):
     for i in range(forest.shape[0]):
-        print([{0: '_', 1: 'T', 2: 'X', 3: 'v'}[cell] for cell in forest[i]])
+        print([{0: ' ', 1: 'T', 2: 'X', 3: '_'}[cell] for cell in forest[i]])
+    print('')
 
 
 def start_fire(forest):
     L = forest.shape[0]
-    state = 0
-    # forest[1][1:L + 1] = [2 if forest[1][i + 1] == 1 else 0 for i in range(L)]
     for i in range(L):
         if forest[0][i] == 1:
             forest[0][i] = 2
-            state += 1
         else:
             forest[0][i] = 0
-    return forest, state
+    return forest
 
 
-def automaton(f1=np.array([]), st0=0):
+def wind_direction(x, y, direction, strength=1):
+    i = strength
+    Dir = {
+        '0': [(x - i, y - i), (x - i, y), (x - i, y + i),
+              (x, y - i), (x, y + i),
+              (x + i, y - i), (x + i, y), (x + i, y + i)],
+        # 'N': [(x - i, y)],
+        # 'S': [(x + i, y)],
+        # 'W': [(x, y - i)],
+        # 'E': [(x, y + i)]
+        'N': [(x - i, y - i), (x - i, y), (x - i, y + i)],
+        'S': [(x + i, y - i), (x + i, y), (x + i, y + i)],
+        'W': [(x - i, y - i), (x, y - i), (x + i, y - i)],
+        'E': [(x - i, y + i), (x, y + i), (x + i, y + i)]
+    }
+    return Dir[direction]
+
+
+def automaton(f1=np.array([]), direction='0', strength=1):
     L = f1.shape[0]
-    st1 = st0
     state_change = True
     f2 = f1.copy()  # forest before and after the move
-    
+
     while state_change is True:
         state_change = False
 
         for x, y in np.ndindex(f1.shape):
+            str0 = strength
+
             if f1[x][y] == 1:  # is tree
-                for i, j in [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
-                             (x, y - 1), (x, y + 1),
-                             (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)]:
-                    if 0 <= i < L and 0 <= j < L:  # if in grid
-                        if f1[i][j] == 2:  # if burning
-                            f2[x][y] = 2
-                            st1 += 1
-                            state_change = True
+                while str0 > 0:
+                    wind = wind_direction(x, y, direction, strength=str0)
+                    for i, j in wind:
+                        if 0 <= i < L and 0 <= j < L:  # if neighbour in grid
+                            if f1[i][j] == 2:  # if the neighbour is burning
+                                f2[x][y] = 2
+                                state_change = True
+                    str0 -= 1
+
             elif f1[x][y] == 2:
                 f2[x][y] = 3
-                st1 -= 1
                 state_change = True
-        # print_forest(f1, st1)
+
+        # print_forest(f1)  # TODO remove print
         f1 = f2.copy()
 
     return f2
 
 
 if __name__ == '__main__':
-    forest = make_forest(5, 0.5)
-    print('BEFORE THE FIRE')
-    print_forest(forest)
-    #
-    forest, state = start_fire(forest)
-    print('\nTHE FIRE STARTED')
-    print_forest(forest, state)
-    #
-    forest = automaton(forest, state)
-    print('\nAFTER THE FIRE')
-    print_forest(forest)
+    forest_fire(0.5, 5, 'W', 2)
--- sem2/agent-based modelling/l1/list1.py
+++ sem2/agent-based modelling/l1/list1.py
@@ -12,29 +12,30 @@ def check_if_fire_hit_other_side(forest):
     return 0
 
 
-def probability_that_fire_hits_other_side(p, L=20, MC=10):
+def probability_that_fire_hits_other_side(p, L=20, MC=10, Dir='0', Str=1):
     ans = 0.
     for mc in range(MC):
-        f = forest_fire(p, L)
+        f = forest_fire(p, L, Dir, Str)
         ans += check_if_fire_hit_other_side(f)
     return ans / MC
 
 
-def percolation_threshold(L, P, MC=10):
+def percolation_threshold(L, P, MC=10, Dir='0', Str=1):
     res = np.zeros(len(P))
     i = 0
     for p in P:
-        res[i] = probability_that_fire_hits_other_side(p, L, MC)
+        res[i] = probability_that_fire_hits_other_side(p, L, MC, Dir=Dir, Str=Str)
         i += 1
     plt.plot(P, res)
-    plt.title('Percolation threshold')
+    plt.title('Percolation threshold for L = ' + str(L))
     plt.xlabel('p')
     plt.ylabel('hit probability')
-    plt.savefig('percolation' + str(L))
+    plt.savefig('percolation_L' + str(L) + '_MC' + str(MC) + '_Dir' + str(Dir) + '_Str' + str(Str))
+    plt.clf()
 
 
 def hoshen_kopelman_alg(forest=np.array([])):
-    # print_forest(forest)
+    # print_forest(forest) TODO remove this print
     L = forest.shape[0]
     label_no = 0
     labels = np.zeros([L, L])
@@ -43,27 +44,21 @@ def hoshen_kopelman_alg(forest=np.array([])):
             above = forest[x - 1][y] if 0 <= x - 1 else 0
             left = forest[x][y - 1] if 0 <= y - 1 else 0
 
-            if above == 0 and left == 0:
+            if above != 3 and left != 3:
                 label_no += 1
                 labels[x][y] = label_no
-            elif above == 3 and left == 0:
+            elif above == 3 and left != 3:
                 labels[x][y] = labels[x - 1][y]
-            elif above == 0 and left == 3:
+            elif above != 3 and left == 3:
                 labels[x][y] = labels[x][y - 1]
-            elif above == 3 and left == 3:  # above and left both have labels (different)
+            elif above == 3 and left == 3:  # above and left both have labels (burned tree)
                 if labels[x - 1][y] != labels[x][y - 1]:
                     union(labels, labels[x - 1][y], labels[x][y - 1])
                 labels[x][y] = labels[x - 1][y]
-    # count labels
-    end_labels = [*labels.flatten()]
-    biggest_cluster = dict((x, end_labels.count(x)) for x in set(end_labels) - {0})
-    # return number of nodes in the biggest cluster
-    # B = max(biggest_cluster.values() if len([*biggest_cluster.keys()]) > 0 else 0)
-    if len(biggest_cluster) > 0:
-        B = max(biggest_cluster.values())
-    else:
-        B = 0
-    return B
+
+    final_labels = [*labels.flatten()]
+    clusters = dict((x, final_labels.count(x)) for x in set(final_labels) - {0})  # count labels
+    return max(clusters.values()) if len(clusters) > 0 else 0  # return number of nodes in the biggest cluster
 
 
 def union(labels, l1, l2):
@@ -73,29 +68,38 @@ def union(labels, l1, l2):
             labels[x][y] = l1
 
 
-def avg_biggest_cluster(L, P, MC=10):
+def avg_biggest_cluster(L, P, MC=10, Dir='0', Str=1):
     # hoshen_kopelman for L=100, monte carlo for different p
-    tmp = np.zeros(MC)
-    res = []
+    res = np.zeros(len(P))
+    i = 0
     for p in P:
-        for mc in range(MC):
-            f = forest_fire(p, L)
-            tmp[mc] = hoshen_kopelman_alg(f)
-        res.append(tmp.mean())
+        tmp = 0.
+        for _ in range(MC):
+            f = forest_fire(p, L, Dir, Str)
+            tmp += hoshen_kopelman_alg(f)
+        res[i] = tmp / MC
+        i += 1
     plt.plot(P, res)
-    plt.title('Biggest cluster of burned trees for grid size = ' + str(L))
+    plt.title('Biggest cluster of burned trees for L=' + str(L) + ', direction ' + Dir + 'and strength ' + str(Str))
     plt.xlabel('p')
     plt.ylabel('number of trees')
-    plt.savefig('biggest_cluster_L100')
+    plt.savefig('biggest_cluster_L100_MC' + str(MC) + '_Dir' + str(Dir) + '_Str' + str(Str))
+    plt.clf()
 
 
 if __name__ == "__main__":
     # print(percolation_threshold(0.5, 5, 10))
     # f = forest_fire(0.5, 5)
     # print(hoshen_kopelman_alg(f))
-    L = 10
-    P = [x for x in np.arange(0, 1., .1)]
+    MC = 10
+    P = [x for x in np.arange(.1, 1., .1)]
 
-    for l in [20, 50, 100]:
-        percolation_threshold(L=l, P=P, MC=10)
-    avg_biggest_cluster(L=L, P=P, MC=10)
+    # L = [20, 50, 100]
+    L = [3, 5, 10]
+    Direction = ['0', 'N', 'W']
+    Strength = [1, 5, 10]
+    for d in Direction:
+        for s in Strength:
+            for l in L:
+                percolation_threshold(L=l, P=P, MC=MC, Dir=d, Str=s)
+            avg_biggest_cluster(L=L[-1], P=P, MC=MC, Dir=d, Str=s)
