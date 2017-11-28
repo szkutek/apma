e25fa39f9ec185fa1b8a628e34609d0f0a961dea
szkutek <aga.szkutek@gmail.com>
2017-11-28 04:06:42 +0100

    abm l4, more results

--- sem2/agent-based modelling/l4/list4.py
+++ sem2/agent-based modelling/l4/list4.py
@@ -3,54 +3,83 @@ import timeit
 from schelling import *
 
 
-def ex2(M):
+def ex2():
     for i in range(5):
-        starting_grid, final_grid, cycles, agents_coord = schelling(M, 500, 250, jb=.5, mb=1, jr=.5, mr=1)
-        save_schelling_plot(starting_grid, 'Schelling - starting positions of agents', 'ex1-begin-' + str(i))
-        save_schelling_plot(final_grid, 'Schelling - final positions of agents', 'ex1-end-' + str(i))
+        starting_grid, final_grid, cycles, agents_coord = schelling(100, 500, 250, jb=.5, mb=11, jr=.5, mr=11)
+        save_schelling_plot(starting_grid, 'Schelling - starting positions of agents', 'ex1-' + str(i) + '-begin')
+        save_schelling_plot(final_grid, 'Schelling - final positions of agents', 'ex1-' + str(i) + '-end')
 
 
-def ex3(M):
+def ex3():
     population_size = np.arange(250, 4050, 50)
     number_of_iterations = np.zeros(len(population_size))
+    execution_times = np.zeros(len(population_size))
+
     for i, Nb in enumerate(population_size):
         N = 2 * Nb
-        number_of_iterations[i] = schellings_number_of_iterations(M, N, Nb, jb=.5, mb=1, jr=.5, mr=1)
+        start = timeit.default_timer()
+        number_of_iterations[i] = schellings_number_of_iterations(100, N, Nb, jb=.5, mb=1, jr=.5, mr=1)
+        execution_times[i] = timeit.default_timer() - start
 
-    plt.plot(population_size, number_of_iterations)
+    plt.scatter(population_size, number_of_iterations)
     plt.title('Number of iterations as a function of population size')
+    plt.ylabel('number of iterations')
+    plt.xlabel('population size')
     plt.savefig('ex3-number-of-iterations.png')
+    plt.clf()
+
+    plt.scatter(population_size, execution_times)
+    plt.title('Execution time as a function of population size')
+    plt.ylabel('execution time')
+    plt.xlabel('population size')
+    plt.savefig('ex3-execution-times.png')
     plt.close()
 
 
-def ex5(M):
-    Jt = np.arange(.1, .9, .1)
+def ex4():
+    Jt = np.arange(.1, .8, .1)
     segregation_index = np.zeros(len(Jt))
     for i, jt in enumerate(Jt):
-        segregation_index[i] = schellings_segregation_index(M, 500, 250, jb=jt, mb=1, jr=jt, mr=1)
+        print('ex4: ' + str(jt))
+        start = timeit.default_timer()
+        segregation_index[i] = schellings_segregation_index(100, 500, 250, jb=jt, mb=1, jr=jt, mr=1)
+        print(timeit.default_timer() - start)
 
-    plt.plot(Jt, segregation_index)
+    plt.scatter(Jt, segregation_index)
     plt.title('Segregation index as a function of j_t')
-    plt.savefig('ex5-segregation-index.png')
+    plt.ylabel('segregation index')
+    plt.xlabel('j_t')
+    plt.savefig('ex4-segregation-index-jt.png')
     plt.close()
 
 
-def ex6(M):
-    Mt = np.arange(1, 12)
+def ex5():
+    Mt = np.arange(1, 12)  # corresponding to 8,24,48,80 and 120 neighbours
     segregation_index = np.zeros(len(Mt))
     for i, mt in enumerate(Mt):
-        segregation_index[i] = schellings_segregation_index(M, 500, 250, jb=.5, mb=mt, jr=.5, mr=mt)
+        print('ex5: ' + str(mt))
+        start = timeit.default_timer()
+        segregation_index[i] = schellings_segregation_index(100, 500, 250, jb=.5, mb=mt, jr=.5, mr=mt)
+        print(timeit.default_timer() - start)
 
-    plt.plot(Mt, segregation_index)
+    plt.scatter(Mt, segregation_index)
     plt.title('Segregation index as a function of m_t')
-    plt.savefig('ex6-segregation-index.png')
+    plt.ylabel('segregation index')
+    plt.xlabel('m_t')
+    plt.savefig('ex5-segregation-index-mt.png')
     plt.close()
+
+
+def ex6():
+    for i in range(5):
+        starting_grid, final_grid, cycles, agents_coord = schelling(100, 2000, 1000, jb=3 / 8, mb=1, jr=6 / 8, mr=1)
+        save_schelling_plot(starting_grid, 'Schelling - starting positions of agents', 'ex6-' + str(i) + '-begin')
+        save_schelling_plot(final_grid, 'Schelling - final positions of agents', 'ex6-' + str(i) + '-end')
 
 
 if __name__ == '__main__':
-    m = 100
-    imp = 'from __main__ import '
-    num = 1
-
-    fun = 'ex5'
-    print(timeit.timeit(fun + '(' + str(m) + ')', imp + fun, number=num))
+    # ex2()
+    # ex3()
+    ex4()
+    print('-------------')
+    ex5()
--- sem2/agent-based modelling/l4/schelling.py
+++ sem2/agent-based modelling/l4/schelling.py
@@ -26,23 +26,21 @@ def distribute_agents(M, N, Nb):
     return arr, blue_coord, red_coord
 
 
-def count_neighbours(state, x, y, m):
+def count_neighbours(state, x, y, mb, mr):
     N, M = state.shape
+    m = mb if state[x][y] == 1 else mr
     nbrs = [(i % N, j % M)
             for i in range(x - m, x + m + 1)
             for j in range(y - m, y + m + 1)
             if not (i == x and j == y)]
     blue = 0
     red = 0
-    empty = 0
     for i, j in nbrs:
         if state[i][j] == 1:
             blue += 1
         elif state[i][j] == 2:
             red += 1
-        else:
-            empty += 1
-    return blue, red, empty
+    return blue, red
 
 
 def move_agent(lattice, c1, c2):
@@ -67,32 +65,32 @@ def schelling_algorithm(lattice, agents_coord, jb, mb, jr, mr):
         changed = False
         for c in range(len(agents_coord)):
             c1, c2 = agents_coord[c]
-            blue_nb, red_nb, empty_nb = count_neighbours(lattice, c1, c2, mb)
+            blue_nb, red_nb = count_neighbours(lattice, c1, c2, mb, mr)
             all_nb = blue_nb + red_nb
-            if all_nb == 0 or (all_nb > 0
-                               and ((lattice[c1][c2] == 1 and blue_nb / all_nb <= jb)
-                                    or (lattice[c1][c2] == 2 and red_nb / all_nb <= jr))):
+            if all_nb == 0 or ((lattice[c1][c2] == 1 and blue_nb / all_nb <= jb)
+                               or (lattice[c1][c2] == 2 and red_nb / all_nb <= jr)):
                 lattice, new_c1, new_c2 = move_agent(lattice, c1, c2)  # moves agents on the lattice
                 agents_coord[c] = [new_c1, new_c2]
                 changed = True
         cycles += 1
-
     return lattice, cycles, agents_coord
 
 
-def segr_index(grid, agent_coord):
+def segr_index(grid, agent_coord, mb, mr):
     index = 0.
+    # mb, mr = 1, 1
     for c1, c2 in agent_coord:
-        blue_nb, red_nb, empty_nb = count_neighbours(grid, c1, c2, 1)
+        blue_nb, red_nb = count_neighbours(grid, c1, c2, mb, mr)
         all_nb = blue_nb + red_nb
-        if grid[c1][c2] == 1:
-            index += blue_nb / all_nb
-        else:
-            index += red_nb / all_nb
+        if all_nb > 0:
+            if grid[c1][c2] == 1:
+                index += blue_nb / all_nb
+            else:
+                index += red_nb / all_nb
     return index / len(agent_coord)
 
 
-def schelling(M, N, Nb, jb=.5, mb=1, jr=.5, mr=1):
+def schelling(M, N, Nb, jb, mb, jr, mr):
     starting_grid, blue_coord, red_coord = distribute_agents(M=M, N=N, Nb=Nb)
     agents_coord = np.array(blue_coord + red_coord)
     final_grid, cycles, agents_coord = schelling_algorithm(starting_grid.copy(), agents_coord,
@@ -100,15 +98,14 @@ def schelling(M, N, Nb, jb=.5, mb=1, jr=.5, mr=1):
     return starting_grid, final_grid, cycles, agents_coord
 
 
-def schellings_number_of_iterations(M, N, Nb, jb=.5, mb=1, jr=.5, mr=1):
+def schellings_number_of_iterations(M, N, Nb, jb, mb, jr, mr):
     starting_grid, final_grid, cycles, agents_coord = schelling(M, N, Nb, jb, mb, jr, mr)
     return cycles
 
 
-def schellings_segregation_index(M, N, Nb, jb=.5, mb=1, jr=.5, mr=1):
+def schellings_segregation_index(M, N, Nb, jb, mb, jr, mr):
     starting_grid, final_grid, cycles, agents_coord = schelling(M, N, Nb, jb, mb, jr, mr)
-    index = segr_index(final_grid, agents_coord)
-    return index
+    return segr_index(final_grid, agents_coord, mb, mr)
 
 
 def save_schelling_plot(lattice, title='schelling', filename='schelling'):
@@ -124,5 +121,5 @@ def save_schelling_plot(lattice, title='schelling', filename='schelling'):
 if __name__ == '__main__':
     grid1, grid2, cycles, agents_coord = schelling(100, 500, 250)
 
-    save_schelling_plot(grid1, 'Schelling - starting positions of agents', 'm1000-n500-b250-start')
-    save_schelling_plot(grid2, 'Schelling - final positions of agents', 'm1000-n500-b250-end')
+    save_schelling_plot(grid1, 'Schelling - starting positions of agents', 'm100-n500-b250-start')
+    save_schelling_plot(grid2, 'Schelling - final positions of agents', 'm100-n500-b250-end')
