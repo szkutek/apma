a59a52cb4d485ad7b2543fb054fda30c645a69a9
szkutek <aga.szkutek@gmail.com>
2018-01-02 23:01:18 +0100

    boids v2

--- sem2/agent-based modelling/l6/boids.py
+++ sem2/agent-based modelling/l6/boids.py
@@ -3,28 +3,21 @@ import matplotlib.pyplot as plt
 import imageio
 
 
-class Boid:
-    def __init__(self, n, x, y, vx, vy):
-        self.n = n
-        self.pos = np.array([x, y])
-        self.vel = np.array([vx, vy])
-
-
 def init_pos(bnum, N):
-    boids = [0] * bnum
+    boids_pos = [0] * bnum
+    boids_vel = [0] * bnum
     for n in range(bnum):
-        boids[n] = Boid(n, np.random.randint(0, N), np.random.randint(0, N), 0., 0.)
-    return boids
+        boids_pos[n] = np.array([np.random.rand() * N, np.random.rand() * N])
+        boids_vel[n] = np.array([2., 1.])
+    return boids_pos, boids_vel
 
 
-def rule1(boid, boids):
+def rule1(bj, boids_pos):
     """Rule 1: Boids try to fly towards the centre of mass of neighbouring boids. """
-    pc = np.array([0., 0.])
-    for b in boids:
-        pc += b.pos
-    pc -= boid.pos
-    pc = pc / (len(boids) - 1)
-    return (pc - boid.pos) / 100
+    pc = sum(boids_pos)
+    pc -= bj
+    pc = pc / (len(boids_pos) - 1)
+    return (pc - bj) / 100
 
 
 def distance(b1, b2):
@@ -32,53 +25,70 @@ def distance(b1, b2):
     return np.sqrt(d)
 
 
-def rule2(boid, boids):
+def rule2(bj, boids_pos):
     """ Rule 2: Boids try to keep a small distance away from other objects (including other boids). """
     c = np.array([0., 0.])
-    for b in boids:
-        if distance(b.pos, boid.pos) < 1:
-            c -= b.pos - boid.pos
+    for pos in boids_pos:
+        if distance(pos, bj) < 2:
+            c -= pos - bj
     return c
 
 
-def rule3(boid, boids):
+def rule3(bv, boids_vel):
     """ Rule 3: Boids try to match velocity with near boids. """
-    pv = np.array([0., 0.])
-    for b in boids:
-        pv += b.vel
-    pv -= boid.vel
-    pv /= (len(boids) - 1)
-    return (pv - boid.vel) / 8
+    pv = sum(boids_vel)  # perceived velocity
+    pv -= bv
+    pv /= (len(boids_vel) - 1)
+    return (pv - bv) / 8
 
 
-def move_boids(boids, N):
-    new_boids = [0] * len(boids)
-    for i in range(len(boids)):
-        b = boids[i]
+def bound_position(bj, N):
+    xmin, xmax, ymin, ymax = 10, N - 10, 10, N - 10
+    vx, vy = 0., 0.
+    x, y = bj
 
-        v1 = rule1(b, boids)
-        v2 = rule2(b, boids)
-        v3 = rule3(b, boids)
-        new_vel = b.vel + v1 + v2 + v3
-        new_pos = b.pos + b.vel
+    if x < xmin:
+        vx = 1
+    elif x > xmax:
+        vx = -1
 
-        new_boids[i] = Boid(i, new_pos[0], new_pos[1], new_vel[0], new_vel[1])
+    if y < ymin:
+        vy = 1
+    elif y > ymax:
+        vy = -1
 
-    return new_boids
+    return np.array([vx, vy])
 
 
-def draw_boids(boids, index):
-    plt.xlim(0, N)
-    plt.ylim(0, N)
-    for b in boids:
-        plt.plot(b.pos[0], b.pos[1], '.')
+def move_boids(boids_pos, boids_vel, bnum, N):
+    new_boids_pos = boids_pos.copy()
+    new_boids_vel = boids_vel.copy()
+    for i in range(bnum):
+        v1 = rule1(boids_pos[i], boids_pos)
+        v2 = rule2(boids_pos[i], boids_pos)
+        v3 = rule3(boids_vel[i], boids_vel)
+        v4 = bound_position(boids_pos[i], N)
+        new_vel = boids_vel[i] + v1 + v2 + v3 + v4
+        new_pos = boids_pos[i] + new_vel
 
-    s = 'b' + "_" + str(i) + '.png'
+        new_boids_pos[i] = new_pos
+        new_boids_vel[i] = new_vel
+
+    return new_boids_pos, new_boids_vel
+
+
+def draw_boids(boids_pos, index):
+    plt.xlim(0, grid_size)
+    plt.ylim(0, grid_size)
+    x, y = list(zip(*boids_pos))
+    plt.plot(x, y, '.')
+
+    s = 'b' + "_" + str(index) + '.png'
     plt.savefig(s)
     plt.clf()
 
 
-def movie(n, pathname, moviename, duration=0.2):
+def movie(n, pathname, moviename, duration=0.3):
     frames = []
     for i in range(n):
         path = pathname + "_" + str(i) + '.png'
@@ -89,15 +99,15 @@ def movie(n, pathname, moviename, duration=0.2):
 
 
 if __name__ == '__main__':
-    N = 10
+    grid_size = 100
     bnum = 10
 
-    boids = init_pos(bnum, N)
+    boids_pos, boids_vel = init_pos(bnum, grid_size)
     plt.figure()
 
     M = 100
     for i in range(M):
-        draw_boids(boids, i)
-        boids = move_boids(boids, N)
+        draw_boids(boids_pos, i)
+        boids_pos, boids_vel = move_boids(boids_pos, boids_vel, bnum, grid_size)
 
     movie(M, 'b', 'boids1')
