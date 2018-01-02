632404d1d328139acfa16431c278ec6827441093
szkutek <aga.szkutek@gmail.com>
2018-01-03 00:20:19 +0100

    boids v3

--- sem2/agent-based modelling/l6/boids.py
+++ sem2/agent-based modelling/l6/boids.py
@@ -4,19 +4,17 @@ import imageio
 
 
 def init_pos(bnum, N):
-    boids_pos = [0] * bnum
-    boids_vel = [0] * bnum
-    for n in range(bnum):
-        boids_pos[n] = np.array([np.random.rand() * N, np.random.rand() * N])
-        boids_vel[n] = np.array([2., 1.])
+    boids_pos = [np.array([np.random.rand() * N, np.random.rand() * N]) for _ in range(bnum)]
+    boids_vel = [np.array([1, 2]) for _ in range(bnum)]
     return boids_pos, boids_vel
 
 
 def rule1(bj, boids_pos):
     """Rule 1: Boids try to fly towards the centre of mass of neighbouring boids. """
-    pc = sum(boids_pos)
-    pc -= bj
-    pc = pc / (len(boids_pos) - 1)
+    tmp = [pos for pos in boids_pos if distance(bj, pos) < 20]
+    n = len(tmp) - 1
+    pc = (sum(tmp) - bj) / n if n > 0 else bj
+
     return (pc - bj) / 100
 
 
@@ -25,20 +23,30 @@ def distance(b1, b2):
     return np.sqrt(d)
 
 
-def rule2(bj, boids_pos):
+def rule2(bj, boids_pos, obstacles):
     """ Rule 2: Boids try to keep a small distance away from other objects (including other boids). """
+
     c = np.array([0., 0.])
     for pos in boids_pos:
-        if distance(pos, bj) < 2:
-            c -= pos - bj
+        if distance(pos, bj) < 5:
+            c -= (pos - bj) / 2
+    for obj in obstacles:
+        if distance(obj, bj) < 5:
+            c -= (obj - bj)
     return c
 
 
-def rule3(bv, boids_vel):
-    """ Rule 3: Boids try to match velocity with near boids. """
-    pv = sum(boids_vel)  # perceived velocity
+def rule3(bj, bv, boids_pos, boids_vel):
+    """ Rule 3: Boids try to match velocity with neighbouring boids. """
+    pv = np.array([0., 0.])  # perceived velocity
+    n = 0
+    for pos, vel in zip(boids_pos, boids_vel):
+        if distance(bj, pos) < 20:
+            pv += vel
+            n += 1
     pv -= bv
-    pv /= (len(boids_vel) - 1)
+    n -= 1
+    pv = pv / n if n > 0 else pv
     return (pv - bv) / 8
 
 
@@ -60,13 +68,13 @@ def bound_position(bj, N):
     return np.array([vx, vy])
 
 
-def move_boids(boids_pos, boids_vel, bnum, N):
+def move_boids(boids_pos, boids_vel, bnum, N, obstacles):
     new_boids_pos = boids_pos.copy()
     new_boids_vel = boids_vel.copy()
     for i in range(bnum):
         v1 = rule1(boids_pos[i], boids_pos)
-        v2 = rule2(boids_pos[i], boids_pos)
-        v3 = rule3(boids_vel[i], boids_vel)
+        v2 = rule2(boids_pos[i], boids_pos, obstacles)
+        v3 = rule3(boids_pos[i], boids_vel[i], boids_pos, boids_vel)
         v4 = bound_position(boids_pos[i], N)
         new_vel = boids_vel[i] + v1 + v2 + v3 + v4
         new_pos = boids_pos[i] + new_vel
@@ -77,9 +85,12 @@ def move_boids(boids_pos, boids_vel, bnum, N):
     return new_boids_pos, new_boids_vel
 
 
-def draw_boids(boids_pos, index):
+def draw_boids(boids_pos, index, obstacles):
     plt.xlim(0, grid_size)
     plt.ylim(0, grid_size)
+    x, y = list(zip(*obstacles))
+    plt.plot(x, y, '.r')
+
     x, y = list(zip(*boids_pos))
     plt.plot(x, y, '.')
 
@@ -88,7 +99,7 @@ def draw_boids(boids_pos, index):
     plt.clf()
 
 
-def movie(n, pathname, moviename, duration=0.3):
+def movie(n, pathname, moviename, duration=0.2):
     frames = []
     for i in range(n):
         path = pathname + "_" + str(i) + '.png'
@@ -106,8 +117,10 @@ if __name__ == '__main__':
     plt.figure()
 
     M = 100
+    obstacles = [np.array([np.random.rand() * grid_size, np.random.rand() * grid_size]) for _ in range(5)]
+
     for i in range(M):
-        draw_boids(boids_pos, i)
-        boids_pos, boids_vel = move_boids(boids_pos, boids_vel, bnum, grid_size)
+        draw_boids(boids_pos, i, obstacles)
+        boids_pos, boids_vel = move_boids(boids_pos, boids_vel, bnum, grid_size, obstacles)
 
     movie(M, 'b', 'boids1')
