f8bd6667511c21d509c8c6f5c85c4190938c6460
szkutek <aga.szkutek@gmail.com>
2018-01-15 01:43:13 +0100

    abm l7 final

--- sem2/agent-based modelling/l7/qvoter.py
+++ sem2/agent-based modelling/l7/qvoter.py
@@ -68,21 +68,17 @@ def new_graph(g, N, M):
 #
 #     return timeline
 
-def MCsimulations(g, M, f, MC=100, MC2=1):
+def MCsimulations(g, M, f, P, MC=100, MC2=1):
     N = 100
     q = 4
-    P = np.arange(0.1, 1, 0.1)
+    magnP = np.zeros(len(P), float)
 
-    magnP = []
-
-    for p in P:
+    for en, p in enumerate(P):
         final_magn = 0
         for _ in range(MC2):
             G = new_graph(g, N, M)
             votes = np.ones(N, int)
 
-            timeline = np.zeros(MC)
-            timeline[0] = 1.
             for i in range(MC - 1):
                 for _ in range(N - 1):
                     voter = rnd.randint(0, N - 1)
@@ -95,29 +91,27 @@ def MCsimulations(g, M, f, MC=100, MC2=1):
                         subset_votes = [votes[rnd.choice(neighbours)] for _ in range(q)]
                         if abs(sum(subset_votes)) == q:
                             votes[voter] = subset_votes[0]
-                timeline[i + 1] = sum(votes) / N
 
-            final_magn += timeline[-1]
+            final_magn += sum(votes) / N
 
-        magnP.append(final_magn / MC2)
+        magnP[en] = final_magn / MC2
     return magnP
 
 
-def situation_model_M(MC, rep):
+def situation_model_M(P, MC, rep):
     # MM = [2, 3, 4, 6, 8, 10]
     MM = [2, 3]
     f = 0.5
-    P = np.arange(0.1, 1, 0.1)
 
     plt.figure()
     for M in MM:
-        res = MCsimulations('ba', M, f, MC, rep)
+        res = MCsimulations('ba', M, f, P, MC, rep)
         plt.plot(P, res, '.')
 
-    res = MCsimulations('complete', 0, f, MC, rep)
+    res = MCsimulations('complete', 0, f, P, MC, rep)
     plt.plot(P, res, '.')
 
-    legend = MM + ['CG']
+    legend = ["M=" + str(M) for M in MM] + ['CG']
     plt.legend(legend)
     plt.xlabel("p")
     plt.ylabel("c")
@@ -126,17 +120,16 @@ def situation_model_M(MC, rep):
     plt.close()
 
 
-def situation_model_f(MC, rep):
-    P = np.arange(0.1, 1, 0.1)
+def situation_model_f(P, MC, rep):
     M = 4
     F = np.arange(0.2, 0.6, 0.1)
 
     plt.figure()
     for f in F:
-        res = MCsimulations('ba', M, f, MC, rep)
+        res = MCsimulations('ba', M, f, P, MC, rep)
         plt.plot(P, res, '.')
 
-    legend = F
+    legend = ["f=" + str(f) for f in F]
     plt.legend(legend)
     plt.xlabel("p")
     plt.ylabel("c")
@@ -146,16 +139,17 @@ def situation_model_f(MC, rep):
 
 
 if __name__ == "__main__":
-    MC = 100
-    rep = 10
+    MC = 1000
+    rep = 1000
+    P = np.arange(0.1, 1, 0.02)
 
     start = timer()
 
     print("M:")
-    situation_model_M(MC, rep)
+    situation_model_M(P, MC, rep)
     print(timer() - start)
 
     start = timer()
     print("f:")
-    situation_model_f(MC, rep)
+    situation_model_f(P, MC, rep)
     print(timer() - start)
