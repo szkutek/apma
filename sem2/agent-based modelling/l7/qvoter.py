3871edd3e3e8be299b57c491f820569d1aa8f78b
szkutek <aga.szkutek@gmail.com>
2018-01-15 01:44:51 +0100

    abm l7 final

--- sem2/agent-based modelling/l7/qvoter.py
+++ sem2/agent-based modelling/l7/qvoter.py
@@ -6,67 +6,12 @@ import pylab
 from timeit import default_timer as timer
 
 
-def save_graph(G, pathname, i):
-    fig = pylab.figure()
-    pos = nx.spring_layout(G)
-    size = 200
-
-    green = []
-    red = []
-    for node in G.nodes():
-        if G.node[node]['vote']:
-            green.append(node)
-        else:
-            red.append(node)
-
-    nx.draw_networkx_nodes(G, pos, nodelist=green, node_color='g', node_size=size, alpha=0.8)
-    nx.draw_networkx_nodes(G, pos, nodelist=red, node_color='r', node_size=size, alpha=0.8)
-
-    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
-
-    s = pathname + "_" + str(i) + ".png"
-    pylab.savefig(s)
-    pylab.close(fig)
-
-
 def new_graph(g, N, M):
     if g == 'complete':
         return nx.complete_graph(N)
     elif g == 'ba':
         return nx.barabasi_albert_graph(N, M)
 
-
-# def MCsimulations(g, N, P, q=3, MC=100, MC2=1):
-#     magnP = []
-#     timeP = []
-#     # nodes = np.arange(0, N, 1)
-#
-#     for p in P:
-#         final_magn = 0
-#         timeline0 = np.zeros(MC)
-#         for _ in range(MC2):
-#             timeline = one_timeline_simulation(g, N, p, q, MC)
-#
-#             final_magn += timeline[-1]
-#             timeline0 = np.add(timeline0, timeline)
-#
-#         magnP.append(final_magn / MC2)
-#         timeP.append(np.divide(timeline0, MC2))
-#     return magnP, timeP
-
-
-# def one_timeline_simulation(g, N, p, q, MC):
-#     G = new_graph(g, N)
-#     votes = np.ones(N, int)
-#
-#     timeline = np.zeros(MC)
-#     timeline[0] = 1.
-#     for i in range(MC - 1):
-#         for _ in range(N - 1):
-#             qvoter_NNgroup(G, N, q, p, votes)
-#         timeline[i + 1] = qvoter_NNgroup(G, N, q, p, votes)
-#
-#     return timeline
 
 def MCsimulations(g, M, f, P, MC=100, MC2=1):
     N = 100
@@ -99,8 +44,8 @@ def MCsimulations(g, M, f, P, MC=100, MC2=1):
 
 
 def situation_model_M(P, MC, rep):
-    # MM = [2, 3, 4, 6, 8, 10]
-    MM = [2, 3]
+    MM = [2, 3, 4, 6, 8, 10]
+    # MM = [10]
     f = 0.5
 
     plt.figure()
